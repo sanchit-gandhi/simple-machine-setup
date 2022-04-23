4a28beb42cf3d2d361947ef35869b0da5cc09ab3
sanchit-gandhi <sanchit@huggingface.co>
2022-04-23 17:54:03 +0200

    up

--- run.sh
+++ run.sh
@@ -2,7 +2,6 @@
 ./run_git.sh
 ./run_python_packages.sh
 ./run_python_hf.sh
-./run_bash.sh
 ./run_other_apt.sh
 ./run_seq2seq_speech.sh
 ./run_zsh.sh
--- run_python_hf.sh
+++ run_python_hf.sh
@@ -4,7 +4,8 @@ cd /home/sanchitgandhi
 git clone https://github.com/sanchit-gandhi/transformers.git
 git clone https://github.com/huggingface/datasets.git
 
-cd /home/sanchitgandhi/transformers && git remote add upstream https://github.com/huggingface/transformers.git && pip install -e ".[flax]"
+cd /home/sanchitgandhi/transformers && git remote add upstream https://github.com/huggingface/transformers.git
+cd /home/sanchitgandhi/transformers && pip install -e ".[flax]"
 cd /home/sanchitgandhi/datasets && pip install -e ".[streaming]"
 
 # chex is broken with latest release of JAX - install from head
