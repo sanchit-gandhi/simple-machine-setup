94d2196e6054eabb1d32479fc1e0ac62cb99f5fe
sanchit-gandhi <sanchit@huggingface.co>
2022-05-13 14:05:41 +0100

    up

--- run_python_packages.sh
+++ run_python_packages.sh
@@ -10,3 +10,4 @@ pip install numpy==1.21
 pip install soundfile
 pip install librosa
 pip install torch torchaudio --extra-index-url https://download.pytorch.org/whl/cpu
+wandb login bdc4b72530c041c0a5249301fbb957674970785c
