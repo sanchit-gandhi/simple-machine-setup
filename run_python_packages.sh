e3669bd4fac1a4a08614ea511ce154bf619b058d
sanchit-gandhi <sanchit@huggingface.co>
2023-03-13 09:58:15 +0100

    remove wandb login

--- run_python_packages.sh
+++ run_python_packages.sh
@@ -11,4 +11,3 @@ pip install soundfile
 pip install librosa
 pip install evaluate
 pip install torch torchaudio --extra-index-url https://download.pytorch.org/whl/cpu
-wandb login bdc4b72530c041c0a5249301fbb957674970785c
---
+++ run_whisper_jax.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd /home/sanchitgandhi
+
+git clone https://github.com/sanchit-gandhi/whisper-jax.git
