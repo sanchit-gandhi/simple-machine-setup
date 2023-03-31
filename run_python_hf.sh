cbaebf6506290865c43113db57803a1a6cd328e5
sanchit-gandhi <sanchit@huggingface.co>
2023-03-31 12:21:32 +0100

    up

--- run_python_hf.sh
+++ run_python_hf.sh
@@ -11,3 +11,9 @@ cd /home/sanchitgandhi/datasets && pip install -e ".[streaming]"
 # transformers has jax pinned to 3.9.0 so we need to re-install it with an updated version
 pip install --upgrade "jax[tpu]" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 pip install --upgrade flax optax chex
+
+# librosa (numba) is incompatible with the latest numpy version so we pin it
+pip install numpy==1.23.5
+
+# just pip install evaluate
+pip install --upgrade evaluate
--- run_whisper_jax.sh
+++ run_whisper_jax.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 cd /home/sanchitgandhi
 
 git clone https://github.com/sanchit-gandhi/whisper-jax.git
