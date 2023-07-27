9771788ec46d76848f14802be31b9fd54194c69f
sanchit-gandhi <sanchit@huggingface.co>
2023-07-27 09:38:16 +0100

    python 10

--- run_python.sh
+++ run_python.sh
@@ -2,7 +2,7 @@
 cd /home/sanchitgandhi
 
 sudo apt-get update
-sudo apt-get install python3.8-venv
+sudo apt-get install python3.10-venv
 python3 -m venv hf
 
 source /home/sanchitgandhi/hf/bin/activate
